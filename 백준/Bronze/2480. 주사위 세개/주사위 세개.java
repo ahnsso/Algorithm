fc6c28fa4634912ffc3738f20ec2b3762c5875dd
SSO <asyeon13@naver.com>
2022-09-19 03:51:19 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(a==b && a==c) {    //전부 동일
+            System.out.println(10000+ a*1000);
+        } else if(a!=b && a!=c && b!=c) {    //전부 다름
+            System.out.println(Math.max(a, Math.max(b,c))*100);
+        } else if(a==b || a==c) {    
+            System.out.println(1000+ a*100);
+        } else {    //b==c
+            System.out.println(1000+ b*100);            
+        } 
+    }
+}
