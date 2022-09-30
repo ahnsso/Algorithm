7c454572199c3f8f5188901fd6c002a3a0269b52
SSO <asyeon13@naver.com>
2022-09-30 23:43:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 88 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int ch = sc.next().charAt(0);
-        System.out.println(ch);
+    public static void main(String[] args) throws IOException {    //IOException(java.io 패키지를 import 해야함) 혹은 그냥 Exception(import 없음)
+        int ch = System.in.read();
+        System.out.print(ch);
     }
 }
