84a473cc2ec209f89e10ffe1b402e5f6d6991072
SSO <asyeon13@naver.com>
2022-10-06 21:27:04 +0900

    [Bronze I] Title: 더하기 사이클, Time: 80 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = sc.nextInt();
-        sc.close();
+        int N = Integer.parseInt(br.readLine());
         
         int count = 0;
         int copy = N;
