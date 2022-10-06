9398ce7332416e1eb88b2b8ca369d137d6fdf304
SSO <asyeon13@naver.com>
2022-10-07 01:38:54 +0900

    [Bronze I] Title: 더하기 사이클, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -11,14 +11,10 @@ public class Main {
         int count = 0;
         int copy = N;
         
-        while(true) {
+        do {
             N = ((N%10)*10) + (((N/10)+(N%10))%10);
             count++;
-            
-            if(copy==N) {
-                break;
-            }
-        }
+        } while(copy!=N);
         
         System.out.println(count);
     }
