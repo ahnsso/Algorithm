e4a84325e9fc9c30b77f30ff6bd1077772afe4b8
SSO <asyeon13@naver.com>
2022-10-09 23:51:01 +0900

    [Bronze I] Title: 평균, Time: 88 ms, Memory: 12988 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,24 +1,28 @@
 import java.util.Arrays;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		double arr[] = new double[Integer.parseInt(br.readLine())];
         
-        double arr[] = new double[sc.nextInt()];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Double.parseDouble(st.nextToken());
+		}
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for(int i=0; i<arr.length; i++) {
+			sum += ((arr[i]/arr[arr.length-1])*100);
+		}
         
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = sc.nextDouble();
-        }
-        sc.close();
-        
-        double sum = 0;
-        Arrays.sort(arr);    //오름차순 정렬로 최댓값은 배열의 마지막 원소가 됨.
-        
-        for(int i=0; i<arr.length; i++) {
-            sum += ((arr[i]/arr[arr.length-1])*100);  
-        }
-        
-        System.out.println(sum/arr.length);
-    }
+		System.out.print(sum/arr.length);
+	}
 }
