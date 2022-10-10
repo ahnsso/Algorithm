6658db42c02ff83c156bc7e2ea68eaef0453495c
SSO <asyeon13@naver.com>
2022-10-10 18:34:05 +0900

    [Bronze I] Title: 평균, Time: 80 ms, Memory: 11716 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,28 +1,29 @@
-import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		double arr[] = new double[Integer.parseInt(br.readLine())];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = Double.parseDouble(st.nextToken());
-		}
-		
-		double sum = 0;
-		Arrays.sort(arr);
-		
-		for(int i=0; i<arr.length; i++) {
-			sum += ((arr[i]/arr[arr.length-1])*100);
-		}
+        int N = Integer.parseInt(br.readLine());
         
-		System.out.print(sum/arr.length);
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+        int max = -1;
+        double sum = 0.0;
+        
+        for(int i=0; i<N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            
+            if(value>max) {
+                max=value;
+            }
+            
+            sum+=value;
+        }
+        
+        System.out.println(((sum/max)*100.0)/N);
+    }
 }
