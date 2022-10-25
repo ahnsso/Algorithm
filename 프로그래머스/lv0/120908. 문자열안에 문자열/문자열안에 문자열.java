23ba1e4d2b607f118d29feb388d1bcf95f7b1793
SSO <asyeon13@naver.com>
2022-10-25 22:35:34 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
+        int answer = 1;
         
-        if(str1.contains(str2)==true) {
-            answer = 1;
-        } else {
+        if(!str1.contains(str2)) {
             answer = 2;
         }
         
