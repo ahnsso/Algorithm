2be9309d8a6cc8f572fdd172a44e114bf18b530f
SSO <asyeon13@naver.com>
2022-10-29 04:04:08 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -2,16 +2,12 @@ class Solution {
     public int solution(int price) {
         int answer = 0;
         
-        double five = price*0.95;
-        double ten = price*0.9;
-        double twenty = price*0.8;
-        
         if(price>=100000 && price<300000) {
-            answer = (int)five;
+            answer = (int)(price*0.95);
         } else if(price>=300000 && price<500000) {
-            answer = (int)ten;
+            answer = (int)(price*0.9);
         } else if(price>=500000) {
-            answer = (int)twenty;
+            answer = (int)(price*0.8);
         } else {
             answer = price;
         }
