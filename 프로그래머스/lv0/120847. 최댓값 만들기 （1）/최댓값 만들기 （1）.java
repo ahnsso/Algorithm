70c0e4d16d42f6e4ad4481094570f8ffb4cf6302
SSO <asyeon13@naver.com>
2022-10-30 17:07:51 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,20 +1,15 @@
 class Solution {
     public int solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        
-        int max = answer[0];
+        int answer = 0;
         
         for(int i=0; i<numbers.length; i++) {
             for(int j=0; j<numbers.length; j++) {
                 if(i!=j) {
-                    answer[i] = numbers[i]*numbers[j];
-                    if(max < answer[i]) {
-                        max = answer[i];
-                    }
+                    answer = Math.max(answer, numbers[i] * numbers[j]);
                 }
             }
         }
         
-        return max;
+        return answer;
     }
 }
