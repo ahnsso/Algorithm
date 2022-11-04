737e2deb6ed3cb48f4711aa7a55594569243202a
SSO <asyeon13@naver.com>
2022-11-05 02:43:49 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
+        int answer = n;
         
         for(int i=0; i<t; i++) {
-            n = n+n;
-            answer = n;
+            answer += answer;
         }
         
         return answer;
