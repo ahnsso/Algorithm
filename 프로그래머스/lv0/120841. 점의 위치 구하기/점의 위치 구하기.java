2fd85f9cb35de42c34c89fa8d2f4d4aa4444d845
SSO <asyeon13@naver.com>
2022-11-17 22:36:45 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -5,7 +5,7 @@ class Solution {
         if(dot[0]>0) {
             if(dot[1]>0) answer = 1;
             else answer = 4;
-        } else if(dot[0]<0) {
+        } else {
             if(dot[1]>0) answer = 2;
             else answer = 3;
         }
