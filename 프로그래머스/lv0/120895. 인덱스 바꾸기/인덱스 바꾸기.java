855b34dc9c035e85c2679c749e551a9db3fb4ecc
SSO <asyeon13@naver.com>
2022-11-23 16:30:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        
+        char[] ch = my_string.toCharArray();
+        //System.out.println(ch);
+        
+        ch[num1] = my_string.charAt(num2);
+        ch[num2] = my_string.charAt(num1);
+
+        answer = String.valueOf(ch);
+        
+        return answer;
+    }
+}
