bad1e330849ce5782742b08bf99c4beebe23e30b
SSO <asyeon13@naver.com>
2022-11-24 20:22:39 +0900

    [level 0] Title: 암호 해독, Time: 10.27 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -2,12 +2,9 @@ class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
         
-        char[] ch = cipher.toCharArray();
-        //System.out.print(ch[0]);
-        
         for(int i=0; i<cipher.length(); i++) {
             if((i+1)%code==0) {
-                answer += ch[i];
+                answer += cipher.charAt(i);
             }
         }
                 
