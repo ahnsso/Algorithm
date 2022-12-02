a9b45251433d78cb19045837404c7cbe8cf8f958
SSO <asyeon13@naver.com>
2022-12-02 19:58:11 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.28 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -3,13 +3,9 @@ class Solution {
         String answer = "";
         
         for( int i = 0; i < n; i++ ) {
-            if( i % 2 == 0 ) {
-                answer += "수";
-            } else {
-                answer += "박";
-            }
+            answer += i % 2 == 0 ? "수" : "박";
         }
         
-        return answer;
+        return answer;        
     }
 }
